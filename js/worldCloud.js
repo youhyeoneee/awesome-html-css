34a6d522fcc050d39d3f32eee8330ffe8acabcbf
youhyeoneee <khukyh2018@khu.ac.kr>
2024-01-21 17:52:31 +0900

    fix: 워드 클라우드 zingchart 이용하여 다시 개발

--- js/worldCloud.js
+++ js/worldCloud.js
@@ -1,77 +1,118 @@
-// HTML 요소 선택
-var articleElement = document.getElementById('article');
-// HTML 요소의 텍스트 내용 가져오기
-var textData = articleElement.innerText || articleElement.textContent;
-// 텍스트에서 단어 빈도수 계산
-var wordCount = {};
-var words = textData.split(/\s+/);
+// 워드클라우드 모듈불러오기
+zingchart.MODULESDIR = 'https://cdn.zingchart.com/modules/';
 
-words.forEach(function (word) {
-    word = word.toLowerCase();
-    if (!wordCount[word]) {
-        wordCount[word] = 1;
-    } else {
-        wordCount[word]++;
-    }
-});
+// 많이 나온 단어 많이 뽑기
+function getMostWords(callback) {
+    // HTML 요소 선택
+    var articleElement = document.getElementById('article');
+    // HTML 요소의 텍스트 내용 가져오기
+    var textData = articleElement.innerText || articleElement.textContent;
+    // 텍스트에서 단어 빈도수 계산
+    var wordCount = {};
+    var words = textData.split(/\s+/);
+    var result = [];
 
-// 단어 빈도수를 배열로 변환
-var wordArray = Object
-    .keys(wordCount)
-    .map(function (word) {
-        return {
-            text: word,
-            size: wordCount[word] * 10
-        };
+    words.forEach(function (word) {
+        word = word.toLowerCase();
+
+        if (word.length != 0)
+        {
+            if (!wordCount[word]) {
+                wordCount[word] = 1;
+            } else {
+                wordCount[word]++;
+            }
+        } 
     });
 
-// Word Cloud 레이아웃 설정
-var layout = d3
-    .layout
-    .cloud()
-    .size([800, 400])
-    .words(wordArray)
-    .padding(5)
-    .rotate(function () {
-        return ~~ (Math.random() * 2) * 90;
-    })
-    .font("Impact")
-    .fontSize(function (d) {
-        return d.size;
-    })
-    .on("end", draw);
+    for(var word in wordCount)
+    {
+        var jsonData = {
+            "text" : word,
+            "count" : wordCount[word]
+        }
+        result.push(jsonData);
+    }
 
-// SVG 요소 생성
-var svg = d3
-    .select("#wordCloudContainer")
-    .append("svg")
-    .attr("width", layout.size()[0])
-    .attr("height", layout.size()[1])
-    .append("g")
-    .attr(
-        "transform",
-        "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
-    );
+    console.log(result);
 
-// Word Cloud 그리기
-layout.start();
-
-// 그리기 함수
-function draw(words) {
-    svg
-        .selectAll("text")
-        .data(words)
-        .enter()
-        .append("text")
-        .style("font-size", function (d) {
-            return d.size + "px";
-        })
-        .style("font-family", "Impact")
-        .attr("text-anchor", "middle")
-        .attr("transform", function (d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function (d) {
-            return d.text;
-        });
+    if (typeof callback === 'function') {
+        callback(result);
+    }
 }
+
+//워드클라우드 환경설정
+var myConfig = {
+    type: 'wordcloud',
+    options: {
+        words: [],
+        minLength: 5,
+        ignore: [""],
+        maxItems: 40,
+        aspect: 'spiral', // 'flow-top' | 'flow-center'
+
+        colorType: 'palette',
+        palette: [
+            '#33FFCC',
+            '#003300',
+            '#009966',
+            '#009933',
+            '#99CC99',
+            '#8D6E63',
+            '#5D4037',
+            '#000000',
+            '#333333',
+            '#666666',
+            '#999999',
+            '#CCCCCC',
+        ],
+
+        style: {
+            fontFamily: 'Crete Round',
+
+            hoverState: {
+                backgroundColor: '#D32F2F',
+                borderRadius: 2,
+                fontColor: 'white'
+            },
+            tooltip: {
+                text: '%text: %hits',
+                visible: true,
+                alpha: 0.9,
+                backgroundColor: '#1976D2',
+                borderRadius: 2,
+                borderColor: 'none',
+                fontColor: 'white',
+                fontFamily: 'Georgia',
+                textAlpha: 1
+            }
+        }
+    },
+
+    source: {
+        // text: '--President Barack Obama<br> Selma 50th anniversary speech<br>March 7,
+        // 2015', Source:
+        // https://obamawhitehouse.archives.gov/the-press-office/2015/03/07/remarks-president-50th-anniversary-selma-montgomery-marches
+        fontColor: '#64B5F6',
+        fontSize: 10,
+        fontFamily: 'Georgia',
+        fontWeight: 'normal',
+        marginBottom: '10%'
+    }
+};
+
+// 호출할 콜백 함수
+function renderWordCloud(words) {
+    myConfig.options.words = words;
+
+    // 워드클라우드 렌더링
+    zingchart.render({
+        id: 'wordCloudContainer',
+        data: myConfig,
+        height: 400,
+        width: '100%'
+    });
+}
+
+// getMostWords 함수 호출
+getMostWords(renderWordCloud);
